f0ac47bedfb48e108aa5bde947706d6271526b3b
Dullu <dullus@ds.ad.adp.com>
2016-07-30 12:06:32 +0530

    Testcase

---
+++ TestCase.java
@@ -0,0 +1,12 @@
+package com.predictor;
+
+/**
+ * Created by dullus on 7/28/2016.
+ */
+public class TestCase
+{
+    public static void main(String[] args) {
+        Hand test = new Hand();
+
+    }
+}
